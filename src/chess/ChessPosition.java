3bbabcb8f30e45eab01f54b8ab61d0dd2ef450a9
Lucas Pereira <lucaspsaracaju@gmail.com>
2023-10-09 12:01:14 -0300

    Pequena melhoria na impressão do tabuleiro

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -5,6 +5,7 @@ import boardgame.Position;
 public class ChessPosition {
 	private char column;
 	private int row;
+	
 	public ChessPosition(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating chessPosition. Valid values are from a1 to h8.");
